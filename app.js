0f279b82fa03123d144aafddf68ccd57717196ee
janrhian102904@gmail.com <janrhian012904@gmail.com>
2025-08-27 22:54:07 +0800

    added background blur on form

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
-const buttonPop = document.querySelector('.button-pop');
+const buttonPop = document.querySelectorAll('.button-pop');
 const closePop = document.querySelector('.close');
 
 const formBlur = document.querySelector('.hidden-blur');
+
+const sidebar = document.querySelector('.sidebar');
+const sideBar = document.getElementById('side-bar');
+const sidebarClose = document.getElementById('sidebarclose');
+const hamburger = document.querySelector('.hamburger');
 registerLink.addEventListener('click', () => {
     wrapper.classList.add('active');
 })
@@ -12,12 +17,24 @@ registerLink.addEventListener('click', () => {
     wrapper.classList.remove('active');
 })
 
-buttonPop.addEventListener('click', () => {
-    wrapper.classList.add('active-popup');
-    formBlur.classList.add('active-blur');
-})
+buttonPop.forEach(btn => {
+    btn.addEventListener('click', () => {
+        wrapper.classList.add('active-popup');
+        formBlur.classList.add('active-blur');
+    });
+});
 closePop.addEventListener('click', () => {
     wrapper.classList.remove('active-popup');
     formBlur.classList.remove('active-blur');
 
+})
+
+sideBar.addEventListener('click', () => {
+    sidebar.style.display = 'flex';
+    hamburger.style.display = 'none';
+
+})
+sidebarClose.addEventListener('click', () => {
+    sidebar.style.display = 'none';
+    hamburger.style.display = 'flex';
 })
