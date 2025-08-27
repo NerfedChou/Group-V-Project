efb4e991850071ab7e491278be32e7c901548e55
janrhian102904@gmail.com <janrhian012904@gmail.com>
2025-08-27 18:20:38 +0800

    Initial Commit

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
-
+const buttonPop = document.querySelector('.button-pop');
+const closePop = document.querySelector('.close');
 registerLink.addEventListener('click', () => {
     wrapper.classList.add('active');
 })
 ;loginLink.addEventListener('click', () => {
     wrapper.classList.remove('active');
+})
+
+buttonPop.addEventListener('click', () => {
+    wrapper.classList.add('active-popup');
+})
+closePop.addEventListener('click', () => {
+    wrapper.classList.remove('active-popup');
 })
